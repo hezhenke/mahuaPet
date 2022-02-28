fbcc508eab75f5bc2462817b757dc25659718a8d
ehohe <ehohe@tencent.com>
2022-02-28 17:55:12 +0800

    修改android依赖

---
+++ mahua_pet/android/app/src/main/java/com/mahua/mahua_pet/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mahua.mahua_pet;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- mahua_pet/android/app/src/main/kotlin/com/example/mahua_pet/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mahua_pet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
